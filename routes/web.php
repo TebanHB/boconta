3efec360f76f5b10397f1c74317fb7cc37d099ce
Enrique <carlosenrique753p@gmail.com>
2022-10-17 18:12:12 -0400

    a

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
-
-
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dash', [App\Http\Controllers\HomeController::class, 'index'])->name('dash');
